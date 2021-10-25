823507cd2f96f8adcdb5fe1647e96747d7cbeea6
Eryk Piasecki <eryk.piasecki@aterium.org>
2021-10-25 20:23:28 +0200

    Added translator.py and its test_translator.py, added linting to CircleCI

---
+++ verbcalc/__init__.py
@@ -0,0 +1,3 @@
+from verbcalc.core.translator import Translator
+
+__all__ = Translator()
---
+++ verbcalc/tests/test_translator.py
@@ -0,0 +1,18 @@
+import unittest
+
+import verbcalc
+
+
+class TestTranslator(unittest.TestCase):
+    def setUp(self):
+        self.verbcalc_translator = verbcalc.Translator()
+        self.values = []
+
+    def test_addition(self):
+        self.values.append(self.verbcalc_translator.translate('2 plus 2'))
+        self.values.append(self.verbcalc_translator.translate('2 Plus 2'))
+        self.assertListEqual(['2 + 2', '2 + 2'], self.values)
+
+
+if __name__ == '__main__':
+    unittest.main()
