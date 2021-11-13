8ff85c9bb1590f61fc3ef578c722f8b4d0be57b1
Eryk Piasecki <eryk.piasecki@aterium.org>
2021-11-13 22:09:14 +0100

    Prepared setup.py for new pre-release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setup(
     name='verbcalc',
-    version='v1.0.1-beta',
+    version='v1.1.0-beta',
     packages=['verbcalc', 'verbcalc.core', 'verbcalc.tests'],
     url='https://github.com/ErykPiasecki07/VerbCalc',
     project_urls={
