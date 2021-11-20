4786794bcc5de464aba22561cbbd9b30a7a7cefe
Eryk Piasecki <d4fd8e73-2343-4491-9676-fcfa473ebcea@aleeas.com>
2021-11-20 23:30:07 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setup(
     name='verbcalc',
-    version='v1.3.0-beta',
+    version='v1.3.1-beta',
     packages=['verbcalc', 'verbcalc.core', 'verbcalc.tests'],
     url='https://github.com/ErykPiasecki07/VerbCalc',
     project_urls={
